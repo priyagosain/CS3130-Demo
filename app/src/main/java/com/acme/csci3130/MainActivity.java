6ff72cebdcebce181e1f40b21a3939c93b0a27bf
priyagosain <pr932087@dal.ca>
2017-05-11 15:10:42 -0300

    Hello World being updated dynamically using a click button.

--- app/src/main/java/com/acme/csci3130/MainActivity.java
+++ app/src/main/java/com/acme/csci3130/MainActivity.java
@@ -2,12 +2,36 @@ package com.acme.csci3130;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+    Button button;
+    TextView helloText;
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //get the TextView Object
+        helloText = (TextView) findViewById(R.id.helloText);
+        //get the EditText object
+        editText = (EditText) findViewById(R.id.editText);
+        //get the button object
+        button = (Button) findViewById(R.id.button);
     }
-}
+    public void onClick(View view) {
+        helloText.setText(editText.getText().toString());
+    }
+        }
+
+
+
+
+
+
+
+
